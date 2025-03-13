62f8619ffb8f5ef31410a48712eebf21c048a099
Theis Jensen <53541469+Theis194@users.noreply.github.com>
2025-03-13 23:11:00 +0100

    fixed bug where login and index html is merged

--- bestpass/main.ts
+++ bestpass/main.ts
@@ -36,9 +36,6 @@ server
       headers: { "content-type": "text/html" },
     });
   }, true)
-  .addRoute("GET", "/", async (req) => {
-    return await server.serveStaticFile(req, "./bestpass/public/index.html")
-  })
   .addRoute("GET", "/api/data", async (_req) => {
     return await new Response(JSON.stringify({ message: "Hello, World!"}), {
       headers: { "content-type": "application/json"},
