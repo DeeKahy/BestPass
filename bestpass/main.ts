07224a20b210e307de23fc9c3dac33a982a7dd7d
Theis Jensen <Theis@bmpnj.dk>
2025-03-17 14:02:59 +0100

    Authentication by default

--- bestpass/main.ts
+++ bestpass/main.ts
@@ -9,10 +9,10 @@ import {
 const server = new Http("./bestpass/public");
 
 server
-  .addRoute("GET", "/", getIndex)
-  .addRoute("GET", "/login", getLogin)
-  .addRoute("GET", "/passwords", getPasswords, true)
-  .addRoute("POST", "/api/savenewpassword", postSaveNewPassword, true)
-  .addRoute("POST", "/api/login", postLogin)
-  .addRoute("POST", "/api/logout", postLogout, true)
+  .addRoute("GET", "/", getIndex, false)
+  .addRoute("GET", "/login", getLogin, false)
+  .addRoute("GET", "/passwords", getPasswords)
+  .addRoute("POST", "/api/savenewpassword", postSaveNewPassword)
+  .addRoute("POST", "/api/login", postLogin, false)
+  .addRoute("POST", "/api/logout", postLogout)
   .serve();
